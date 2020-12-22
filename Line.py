7da20dbc784570108bc9f66c6b947fb26220caf9
darrenchooji <darrenchoojuninn@gmail.com>
2020-12-22 05:05:17 -0800

    Added URL Text File

--- Line.py
+++ Line.py
@@ -62,6 +62,7 @@ time.sleep(5)
 
 # Open the input URL using WebView in LINE
 print("Opening "+lineUrl+" using WebView in LINE...")
-adbOpenUrlInWebViewCommand = r'''adb pull $(adb shell uiautomator dump | grep -oP '[^ ]+.xml') /tmp/KeepMemo.xml ; url=$(perl -ne 'printf "%d %d\n", ($1+$3)/2, ($2+$4)/2 if /text="'''+lineUrl+'''"[^>]*bounds="\[(\d+),(\d+)\]\[(\d+),(\d+)\]"/' /tmp/KeepMemo.xml) ; adb shell input tap $url'''
+formattedLineUrl=lineUrl.replace("/", r"\/")
+adbOpenUrlInWebViewCommand = r'''adb pull $(adb shell uiautomator dump | grep -oP '[^ ]+.xml') /tmp/KeepMemo.xml ; url=$(perl -ne 'printf "%d %d\n", ($1+$3)/2, ($2+$4)/2 if /text="'''+formattedLineUrl+'''"[^>]*bounds="\[(\d+),(\d+)\]\[(\d+),(\d+)\]"/' /tmp/KeepMemo.xml) ; adb shell input tap $url'''
 os.system(adbOpenUrlInWebViewCommand)
 print("Opened "+lineUrl+" using WebView in LINE")
--- Testing02.py
+++ Testing02.py
@@ -1,1 +1,13 @@
+from pathlib import Path
+import os, time
 
+home = str(Path.home())
+urlFile = open(home+"/Desktop/AndroidAnomalyDetection/URLs/URLs", "r")
+for url in urlFile:
+    os.putenv("url", url)
+    os.system("adb shell input text $url")
+    time.sleep(5)
+    os.system("adb shell input keyevent 61 ; adb shell input keyevent 61 ; adb shell input keyevent 160")
+    time.sleep(5)
+    os.system("adb shell input keyevent 21 ; adb shell input keyevent 21")
+urlFile.close()
