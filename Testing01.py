8831d7ffd04a6f96b570dea0952aaeb1a1f62466
darrenchooji <darrenchoojuninn@gmail.com>
2020-12-22 22:51:22 -0800

    Minor Modiications

--- Testing01.py
+++ Testing01.py
@@ -11,12 +11,12 @@ for lineUrl in urlFile:
     lineUrl = lineUrl.replace('/', r'\/')
     adbOpenUrlInWebViewCommand = r'''adb pull $(adb shell uiautomator dump | grep -oP '[^ ]+.xml') /tmp/KeepMemo.xml ; url=$(perl -ne 'printf "%d %d\n", ($1+$3)/2, ($2+$4)/2 if /text="''' +lineUrl+ '''"[^>]*bounds="\[(\d+),(\d+)\]\[(\d+),(\d+)\]"/' /tmp/KeepMemo.xml) ; adb shell input tap $url'''
     os.system(adbOpenUrlInWebViewCommand)
-    time.sleep(60)
+    time.sleep(45)
     isBackInKeepMemo = False
     while not isBackInKeepMemo:
         subprocess.Popen("adb logcat -c", shell=True)
         subprocess.Popen("adb shell input keyevent 4", shell=True)
-        time.sleep(15)
+        time.sleep(10)
         verifyInKeepMemo = subprocess.Popen("adb logcat -d ActivityManager:I *:S | grep Killing | grep com.google.android.webview:sandboxed_process0:org.chromium.content.app.SandboxedProcessService", shell=True, stdout=subprocess.PIPE)
         verifyInKeepMemoOutput = verifyInKeepMemo.stdout.read().decode("ascii")
         if verifyInKeepMemoOutput != "":
