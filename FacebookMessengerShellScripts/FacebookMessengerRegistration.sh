b53826994ee75da0d23ba6068c19dde84a06342a
darrenchooji <70890071+darrenchooji@users.noreply.github.com>
2021-02-01 23:47:12 +0800

    Update FacebookMessengerRegistration.sh

--- FacebookMessengerShellScripts/FacebookMessengerRegistration.sh
+++ FacebookMessengerShellScripts/FacebookMessengerRegistration.sh
@@ -1,24 +1,24 @@
-adb shell input text $email
+adb -s $device_id shell input text $email
 sleep 3
-adb shell input keyevent 61
+adb -s $device_id shell input keyevent 61
 sleep 3
-adb shell input text $password
+adb -s $device_id shell input text $password
 sleep 3
-adb shell input keyevent 61 ; adb shell input keyevent 61 ; adb shell input keyevent 66
+adb -s $device_id shell input keyevent 61 ; adb -s $device_id shell input keyevent 61 ; adb -s $device_id shell input keyevent 66
 sleep 10
-adb shell input keyevent 66
+adb -s $device_id shell input keyevent 66
 sleep 5
-adb shell input keyevent 61 ; adb shell input keyevent 61 ; adb shell input keyevent 66
+adb -s $device_id shell input keyevent 61 ; adb -s $device_id shell input keyevent 61 ; adb -s $device_id shell input keyevent 66
 sleep 3
-adb shell input keyevent 61 ; adb shell input keyevent 66
+adb -s $device_id shell input keyevent 61 ; adb -s $device_id shell input keyevent 66
 sleep 3
-adb shell input keyevent 66
+adb -s $device_id shell input keyevent 66
 sleep 7
-adb shell svc wifi disable
+adb -s $device_id shell svc wifi disable
 sleep 3
-adb shell input keyevent KEYCODE_APP_SWITCH
+adb -s $device_id shell input keyevent KEYCODE_APP_SWITCH
 sleep 5
-adb pull $(adb shell uiautomator dump | grep -oP '[^ ]+.xml') /tmp/RecentApps.xml ; closeRecentApps=$(perl -ne 'printf "%d %d\n", ($1+$3)/2, ($2+$4)/2 if /text="Close all"[^>]*bounds="\[(\d+),(\d+)\]\[(\d+),(\d+)\]"/' /tmp/RecentApps.xml) ; adb shell input tap $closeRecentApps
+adb -s $device_id pull $(adb -s $device_id shell uiautomator dump | grep -oP '[^ ]+.xml') /tmp/RecentApps.xml ; closeRecentApps=$(perl -ne 'printf "%d %d\n", ($1+$3)/2, ($2+$4)/2 if /text="Close all"[^>]*bounds="\[(\d+),(\d+)\]\[(\d+),(\d+)\]"/' /tmp/RecentApps.xml) ; adb -s $device_id shell input tap $closeRecentApps
 sleep 5
-adb shell am start -n com.facebook.orca/.auth.StartScreenActivity
+adb -s $device_id shell am start -n com.facebook.orca/.auth.StartScreenActivity
 sleep 15
