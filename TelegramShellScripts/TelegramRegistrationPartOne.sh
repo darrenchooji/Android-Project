5dc56e6d9aa3eff8ac732a840c153e8abf6ff8d9
darrenchooji <70890071+darrenchooji@users.noreply.github.com>
2021-02-01 23:50:07 +0800

    Update TelegramRegistrationPartOne.sh

--- TelegramShellScripts/TelegramRegistrationPartOne.sh
+++ TelegramShellScripts/TelegramRegistrationPartOne.sh
@@ -1,11 +1,11 @@
-adb pull $(adb shell uiautomator dump | grep -oP '[^ ]+.xml') /tmp/TelegramIntroPage.xml ; coords=$(perl -ne 'printf "%d %d\n", ($1+$3)/2, ($2+$4)/2 if /text="Start Messaging"[^>]*bounds="\[(\d+),(\d+)\]\[(\d+),(\d+)\]"/' /tmp/TelegramIntroPage.xml) ; adb shell input tap $coords
+adb -s $device_id pull $(adb -s $device_id shell uiautomator dump | grep -oP '[^ ]+.xml') /tmp/TelegramIntroPage.xml ; coords=$(perl -ne 'printf "%d %d\n", ($1+$3)/2, ($2+$4)/2 if /text="Start Messaging"[^>]*bounds="\[(\d+),(\d+)\]\[(\d+),(\d+)\]"/' /tmp/TelegramIntroPage.xml) ; adb -s $device_id shell input tap $coords
 sleep 3
-for i in `seq 1 3` ; do adb shell input keyevent 67 ; done
+for i in `seq 1 3` ; do adb -s $device_id shell input keyevent 67 ; done
 sleep 3
-adb shell input text 65
+adb -s $device_id shell input text 65
 sleep 3
-adb shell input keyevent 61
+adb -s $device_id shell input keyevent 61
 sleep 3
-adb shell input text $phoneNumber
+adb -s $device_id shell input text $phoneNumber
 sleep 3
-adb shell input keyevent 61 ; adb shell input keyevent 66
+adb -s $device_id shell input keyevent 61 ; adb -s $device_id shell input keyevent 66
