ea5a7c9604c3d1c44b5c61a0b586900c1f2337a4
darrenchooji <70890071+darrenchooji@users.noreply.github.com>
2021-02-01 23:48:01 +0800

    Update LineOpenKeepMemo.sh

--- LineShellScripts/LineOpenKeepMemo.sh
+++ LineShellScripts/LineOpenKeepMemo.sh
@@ -1,3 +1,3 @@
-adb pull $(adb shell uiautomator dump | grep -oP '[^ ]+.xml') /tmp/Chats.xml ; keepmemo=$(perl -ne 'printf "%d %d\n", ($1+$3)/2, ($2+$4)/2 if /text="Keep Memo"[^>]*bounds="\[(\d+),(\d+)\]\[(\d+),(\d+)\]"/' /tmp/Chats.xml) ; adb shell input tap $keepmemo
+adb -s $device_id pull $(adb -s $device_id shell uiautomator dump | grep -oP '[^ ]+.xml') /tmp/Chats.xml ; keepmemo=$(perl -ne 'printf "%d %d\n", ($1+$3)/2, ($2+$4)/2 if /text="Keep Memo"[^>]*bounds="\[(\d+),(\d+)\]\[(\d+),(\d+)\]"/' /tmp/Chats.xml) ; adb -s $device_id shell input tap $keepmemo
 sleep 3
-adb shell input keyevent 61 ; adb shell input keyevent 160
+adb -s $device_id shell input keyevent 61 ; adb -s $device_id shell input keyevent 160
