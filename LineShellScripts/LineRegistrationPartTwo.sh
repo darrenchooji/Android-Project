385c32c350f2d2ffb28a15d264535714314ef092
darrenchooji <70890071+darrenchooji@users.noreply.github.com>
2021-02-01 23:49:12 +0800

    Update LineRegistrationPartTwo.sh

--- LineShellScripts/LineRegistrationPartTwo.sh
+++ LineShellScripts/LineRegistrationPartTwo.sh
@@ -1,19 +1,19 @@
-adb shell input text $line_otp
+adb -s $device_id shell input text $line_otp
 sleep 7
-adb shell input keyevent 61 ; adb shell input keyevent 160
+adb -s $device_id shell input keyevent 61 ; adb -s $device_id shell input keyevent 160
 sleep 3
-adb shell input text $line_password
+adb -s $device_id shell input text $line_password
 sleep 3
-adb shell input keyevent 66 ; adb shell input keyevent 61 ; adb shell input keyevent 66
+adb -s $device_id shell input keyevent 66 ; adb -s $device_id shell input keyevent 61 ; adb -s $device_id shell input keyevent 66
 sleep 7
-adb shell input keyevent 61 ; adb shell input keyevent 61 ; adb shell input keyevent 160
+adb -s $device_id shell input keyevent 61 ; adb -s $device_id shell input keyevent 61 ; adb -s $device_id shell input keyevent 160
 sleep 3
-adb shell input keyevent 61 ; adb shell input keyevent 160
+adb -s $device_id shell input keyevent 61 ; adb -s $device_id shell input keyevent 160
 sleep 3
-adb shell input keyevent 61 ; adb shell input keyevent 61 ; adb shell input keyevent 160
+adb -s $device_id shell input keyevent 61 ; adb -s $device_id shell input keyevent 61 ; adb -s $device_id shell input keyevent 160
 sleep 3
-for i in `seq 1 5`; do adb shell input keyevent 61 ; done
+for i in `seq 1 5`; do adb -s $device_id shell input keyevent 61 ; done
 sleep 3
-adb shell input keyevent 160
+adb -s $device_id shell input keyevent 160
 sleep 3
-adb shell input keyevent 160
+adb -s $device_id shell input keyevent 160
